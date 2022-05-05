ca9c01ac00a2c57a1297f53c16573684086c0f06
Akapulka01 <svavolhov@gmail.com>
2022-05-05 16:43:56 +0300

    Доделал блок формы обратной связи

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
 import '../css/main.css';
 import  './jquery-3.6.0.min'
 
-
-$(document).ready(function (){
-    //весь код выполняется только после загрузки всего кода
-alert('asdasd');
-});
--- templates/main.php
+++ templates/main.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/build/css/main.css">
+
 </head>
 <body>
 
@@ -44,8 +46,8 @@
             <h2 class="cursTitile">Основы протезирования зубов керамическими реставрациями CAD CAM</h2>
             <strong class="cursInfo">Лектор: Левандовский Эдвард Викторович — стоматолог-ортопед, специалист в цифровой стоматологии с опытом работы более 6 лет.</strong>
             <p class="cursPrise">10000 р.</p>
-                <a class="podrobW" href="www.google.ru">Подробнее</a>
-                <a class="zapBlack" href="www.google.com">Записаться</a>
+                <a class=" podrobW" href="www.google.ru">Подробнее</a>
+                <a class="px-2 zapBlack" href="www.google.com">Записаться</a>
             </div>
         </div>
         <a class="cursCard ">
@@ -55,8 +57,58 @@
 
         </a>
     </div>
+    <div class="Our_advantages">
+        <h1>Наши преймущества</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 ">
+                    <img class="info_image " src="https://static.tildacdn.com/tild3633-3161-4230-a636-306663333231/_.png">
+                    <h3>Актуальная информация</h3>
+                    <p>Мы не хотим просто продать Вам курсы наших лекторов, мы занимаемся обучением! В нашем Instagram мы делаем бесплатные вебинары, на сайте мы выкладываем их запись, новости и статьи</p>
+
+                </div>
+                <div class="col-md-6 ">
+                    <img class="info_image " src="https://static.tildacdn.com/tild6663-6136-4461-a463-666337333236/knowledge.png">
+                    <h3>Разноплановость</h3>
+                    <p>Вы только начинаете свое обучение и еще не уверенны в своих силах или Вы опытный специалист и считаете, что все знаете? Избавьтесь от предрассудков! Вы тоже можете повысить свою квалификацию и получить новые знания</p>
+
+                </div>
+            </div>
+            <div class="row  ">
+                <div class="col-md-6">
+                    <img class="info_image " src="https://static.tildacdn.com/tild3139-6335-4862-b962-633835366531/technical-support.png">
+                    <h3>Передовые технологии</h3>
+                    <p>Как говорил известный ученый: любая хороша развитая технология неотличима от магии. Мы используем самое современное оборудование
+                        для работы и обучения!</p>
+
+                </div>
+                <div class="col-md-6 ">
+                    <img class="info_image " src="	https://static.tildacdn.com/tild6263-6532-4364-a532-633032353838/wheel.png">
+                    <h3>Бесплатно</h3>
+                    <p>Пока Вы это читаете, кто-то участвует в розыгрыше бесплатных мест, скидок и других бонусов в нашем Instagram. Не верите? Посмотрите видео-отзывы наших победителей!</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
+<div class="reviews">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-5 ">
+                <h1>Нам важно ваше мнение</h1>
+            </div>
+                <div class=" col-sm-12 col-6 cursCard">
+                    <form>
+                        a
+                    </form>
+                </div>
+    </div>
+
+    </div>
+</div>
 <div class="test"></div>
 <script src="/build/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
